914ab1e06fa6ee679a470a689088ff86774df7da
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-15 15:09:52 +0530

    loginForm added. With SCSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,7 @@
 import { useContext, useEffect, useState } from "react";
-import "./App.css";
 import { SocketContext } from "./contexts/Socket";
 import { SocketContextInterface } from "./types";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Form, Row } from "react-bootstrap";
 
 function App() {
     const { socket, connected } = useContext(
@@ -17,12 +16,30 @@ function App() {
 
     return (
         <>
-            <Container>
-                <Button variant="primary">button 1</Button>
-                <div className="deveve">hello world!</div>
-                <div className="status">
-                    {connected ? "connected" : "not connected"}
-                </div>
+            <Container id="loginForm">
+                <Row>
+                    <h1 className="text-center">MailClient</h1>
+                    <hr />
+                </Row>
+                <Row className="m-3">
+                    <Form>
+                        <Form.Group className="my-2">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter your email"
+                            />
+                        </Form.Group>
+                        <Form.Group className="my-2">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Enter your password"
+                            />
+                        </Form.Group>
+                        <Button variant="secondary w-100">Login</Button>
+                    </Form>
+                </Row>
             </Container>
         </>
     );
