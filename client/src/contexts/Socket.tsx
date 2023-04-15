4e7a73a87c13bb468bb8260e0e7e5c6838b7ff86
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-15 12:12:00 +0530

    made a socket context

---
+++ client/src/contexts/Socket.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { SocketContextInterface, SocketInterface } from "../types";
+import { useSocket } from "../hooks";
+
+const SocketContext = React.createContext<SocketContextInterface | null>(null);
+
+interface PropsInterface {
+    children: React.ReactNode;
+}
+
+const SocketProvider = ({ children }: PropsInterface) => {
+    const socket = useSocket();
+    const [connected, setConnected] = useState<boolean>(socket.connected);
+
+    useEffect(() => {
+        socket.on("disconnect", () => {
+            setConnected(false);
+            console.log("socket disconnected. Connecting again.");
+            socket.connect();
+        });
+
+        socket.on("connect", () => {
+            setConnected(true);
+        });
+
+        socket.onAny((event, ...args) => {
+            console.log(event, ...args);
+        });
+
+        return () => {
+            socket.off("disconnect");
+            socket.off("connect");
+        };
+    }, []);
+
+    return (
+        <SocketContext.Provider value={{ socket }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export default SocketProvider;
+export { SocketContext };
