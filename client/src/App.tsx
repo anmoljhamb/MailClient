cfdeffa9f2c9010ccd89ca8a889093cc510ac909
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-15 15:23:19 +0530

    state variables added, and a handler

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,29 @@
-import { useContext, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
 import { SocketContext } from "./contexts/Socket";
 import { SocketContextInterface } from "./types";
 import { Button, Container, Form, Row } from "react-bootstrap";
 
 function App() {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
     const { socket, connected } = useContext(
         SocketContext
     ) as SocketContextInterface;
 
-    console.log(connected);
+    const isDisabled = email.length === 0 && password.length === 0;
 
     useEffect(() => {
         socket.connect();
     }, []);
+
+    // handlers.
+
+    const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(password);
+    };
 
     return (
         <>
@@ -22,12 +33,18 @@ function App() {
                     <hr />
                 </Row>
                 <Row className="m-3">
-                    <Form>
+                    <Form onSubmit={handleOnSubmit}>
                         <Form.Group className="my-2">
                             <Form.Label>Email</Form.Label>
                             <Form.Control
                                 type="email"
                                 placeholder="Enter your email"
+                                value={email}
+                                onChange={(
+                                    e: ChangeEvent<HTMLInputElement>
+                                ) => {
+                                    setEmail(e.target.value);
+                                }}
                             />
                         </Form.Group>
                         <Form.Group className="my-2">
@@ -35,9 +52,21 @@ function App() {
                             <Form.Control
                                 type="password"
                                 placeholder="Enter your password"
+                                value={password}
+                                onChange={(
+                                    e: ChangeEvent<HTMLInputElement>
+                                ) => {
+                                    setPassword(e.target.value);
+                                }}
                             />
                         </Form.Group>
-                        <Button variant="secondary w-100">Login</Button>
+                        <Button
+                            type="submit"
+                            variant="secondary w-100"
+                            disabled={isDisabled}
+                        >
+                            Login
+                        </Button>
                     </Form>
                 </Row>
             </Container>
