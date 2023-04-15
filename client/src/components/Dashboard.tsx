029e59ef09591dae949f0d2c7d1001c7261143d0
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-15 19:54:59 +0530

    sample layout made for compose a new email

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Container,
+    Row,
+    Col,
+    CloseButton,
+    Form,
+    Button,
+    FloatingLabel,
+} from "react-bootstrap";
+import { GrAdd } from "react-icons/gr";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const [addNew, setAddNew] = useState<boolean>(false);
+
+    return (
+        <>
+            <button
+                id="addNewMail"
+                onClick={() => {
+                    setAddNew(true);
+                }}
+                disabled={addNew}
+            >
+                <GrAdd />
+            </button>
+            {addNew && (
+                <Container id="composeNew">
+                    <Row>
+                        <Col>
+                            <h2>New Message</h2>
+                        </Col>
+                        <Col className="d-flex justify-content-end">
+                            <CloseButton
+                                onClick={() => {
+                                    setAddNew(false);
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                    <hr />
+                    <Row>
+                        <Row>
+                            <Form>
+                                <Form.FloatingLabel label="To" className="my-2">
+                                    <Form.Control
+                                        type="email"
+                                        placeholder="To"
+                                    />
+                                </Form.FloatingLabel>
+                                <Form.FloatingLabel
+                                    label="Subject"
+                                    className="my-2"
+                                >
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Subject"
+                                    />
+                                </Form.FloatingLabel>
+                                <Form.FloatingLabel
+                                    controlId="floatingTextarea2"
+                                    label="Body"
+                                >
+                                    <Form.Control
+                                        as="textarea"
+                                        style={{ height: "10rem" }}
+                                    />
+                                </Form.FloatingLabel>
+                                <Row className="my-2">
+                                    <Col>
+                                        <Button variant="primary">Send</Button>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </Row>
+                    </Row>
+                </Container>
+            )}
+        </>
+    );
 };
 
 export default Dashboard;
