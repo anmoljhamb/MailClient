2a315e8aa33361a032f4c6f677e885c709464df4
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-18 05:11:26 +0530

    fix when sender's name is not given. use their address instead

--- client/src/components/Mail.tsx
+++ client/src/components/Mail.tsx
@@ -11,7 +11,11 @@ const Mail = ({ mail, handleOnClick }: PropsInterface) => {
     return (
         <React.Fragment>
             <Row id="mail" className="my-2" onClick={handleOnClick}>
-                <Col xs={2}>{mail.from?.value.at(0)?.name}</Col>
+                <Col xs={2} style={{ overflowX: "hidden" }}>
+                    {mail.from?.value.at(0)?.name
+                        ? mail.from?.value.at(0)?.name
+                        : mail.from?.value.at(0)?.address}
+                </Col>
                 <Col>
                     {mail.subject} - {mail.text}
                 </Col>
