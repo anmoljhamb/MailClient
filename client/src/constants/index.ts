bff2b22abea73ecf5f84a1e39488f769e42b00bf
Anmol Jhamb <talktoanmol@outlook.com>
2023-04-18 07:35:07 +0530

    fix backend uri constant

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,1 +1,3 @@
-export const BACKEND_URI = "http://localhost:8080";
+const currentUrl = window.location.href;
+const exp = new RegExp(/https?:\/\/([a-zA-Z0-9.]+):[0-9]+/);
+export const BACKEND_URI = `http://${exp.exec(currentUrl)![1]}:8080`;
